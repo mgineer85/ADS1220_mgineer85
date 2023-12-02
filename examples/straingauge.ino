5642ee4e679a68cffb5c8d5b81ed2e3af2e32aec
michael <me@mgrl.de>
2023-12-02 19:35:58 +0100

    add optimized settings for lowest noise

--- examples/straingauge.ino
+++ examples/straingauge.ino
@@ -23,13 +23,20 @@ void setup()
     }
 
     // eco mode, turn off bridge excitation if no measurement
-    ads.setPowerSwitch(ADS1220_PSW_AUTOMATIC);
+    ads.setPowerSwitch(ADS1220_PSW_AUTOMATIC); // send powerdown to open switch
     ads.setConversionMode(ADS1220_CM_SINGLE_SHOT_MODE);
 
-    // setup strain gauge
+    // setup strain gauge config
     ads.setMux(ADS1220_MUX_AIN1_AIN2);
     ads.setVoltageReference(ADS1220_VREF_EXTERNAL_REFP1_REFN1);
     ads.setGain(ADS1220_GAIN_128);
+
+    // optimized for lowest noise
+    ads.setOperatingMode(ADS1220_MODE_TURBO);
+    ads.setDataRate(ADS1220_DR_NORMAL20_DUTY5_TURBO40); // slowest turbo mode. turbo mode and average 4 is lower noise than normal mode average 2.
+    ads.setFirFilter(ADS1220_FIR_50_60_REJECTION_OFF);  // yes, noise without is lower.
+
+    // offset calibration
     ads.internalCalibration();
 }
 
